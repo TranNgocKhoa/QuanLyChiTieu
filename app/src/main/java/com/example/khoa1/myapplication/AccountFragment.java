9bf6088e9c59157ca73d4211bd89765f6cb4e27b
Hirant <lychautrinah@gmail.com>
2017-11-16 14:25:09 +0700

    đổ dữ liệu

--- app/src/main/java/com/example/khoa1/myapplication/AccountFragment.java
+++ app/src/main/java/com/example/khoa1/myapplication/AccountFragment.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,54 +16,101 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.khoa1.myapplication.Adapter.AccountAdapter;
+import com.example.khoa1.myapplication.Database.SQLiteAccount;
+import com.example.khoa1.myapplication.Database.SQLiteDataController;
 import com.example.khoa1.myapplication.Model.Account;
 import com.example.khoa1.myapplication.Model.AccountType;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by khoa1 on 10/30/2017.
  */
+
 public class AccountFragment extends Fragment {
     private ListView lvAccount;
-
+    private ArrayList<Account> listAccount;
     public AccountFragment() {
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
 
+        //Log.d("aaa",Integer.toString(account.size()));
+//        lvAccount = (ListView) rootView.findViewById(R.id.lv_Account);
+//        ArrayAdapter<String> arAdp = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, menuitem);
         lvAccount = (ListView) rootView.findViewById(R.id.lv_Account);
+        //  ArrayList<Account> arrAccount = new ArrayList<Account>();
+        createDB();
+        //Test database
+        Random rand = new Random();
+        int  n = rand.nextInt(10) + 1;
+        AccountType accountType=new AccountType(1,"Agribank","blablabla",null);
+        Account account=new Account(0,Integer.toString(n),10000,2000,null,accountType);
+        final SQLiteAccount account1=new SQLiteAccount(getActivity());
+        account1.addAccount(account);
+        getListaccount();
+        AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, listAccount);
+        lvAccount.setAdapter(accountAdaper);
 
-        lvAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        lvAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(getActivity().getApplicationContext(),ChinhSuaTaiKhoan.class);
+//                startActivity(intent);
+//            }
+//        });
+        lvAccount.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                listAccount.get(position).getName();
+                Log.d("bbb",listAccount.get(position).getName());
+                deleteaccount(getAccountID(listAccount.get(position).getName()));
+                getListaccount();
+                AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, listAccount);
+                lvAccount.setAdapter(accountAdaper);
+                return false;
+            }
+        });
+        lvAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity().getApplicationContext(),ChiTietTaiKhoan.class);
                 startActivity(intent);
             }
         });
-        AccountType tkNganHang = new AccountType(1, "TaiKhoanNganHang", "Tai khoan nam o ngan hang ACB",ContextCompat.getDrawable(getContext(), R.drawable.bank_account) );
-        AccountType tkVi = new AccountType(2, "TaiKhoanVi", "So Tien Trong Vi",ContextCompat.getDrawable(getContext(), R.drawable.wallet_account) );
-        ArrayList<Account> arrAccount = new ArrayList<Account>();
-
-        Account acc1 = new Account(1, "Số tiền dư và nợ trong ví", 1200000, 0, tkVi);
-        Account acc2 = new Account(2, "Ngân Hàng Vietcombank", 1200000, 0, tkNganHang);
-        Account acc3 = new Account(3, "Ngân Hàng ACB", 1200000, 0, tkNganHang);
-        Account acc4 = new Account(4, "Ngân Hàng Techcombank", 1200000, 0, tkNganHang);
-
-        arrAccount.add(acc1);
-        arrAccount.add(acc2);
-        arrAccount.add(acc3);
-        arrAccount.add(acc4);
-
-        AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, arrAccount);
-        lvAccount.setAdapter(accountAdaper);
-//lvAccount.setAdapter(arAdp);
         return rootView;
     }
+    private void createDB() {
+        // khởi tạo database
+        SQLiteDataController sql = new SQLiteDataController(getActivity());
+        try {
+            sql.isCreatedDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void deleteaccount(int position){
+        SQLiteAccount account = new SQLiteAccount(getActivity());
+        account.deleteAccount(position);
+    }
+    private void getListaccount() {
+        SQLiteAccount account = new SQLiteAccount(getActivity());
+        listAccount = new ArrayList<>();
+        listAccount = account.getListAccount();
 
-
+    }
+    private  int getAccountID(String name) {
+        SQLiteAccount account = new SQLiteAccount(getActivity());
+        return account.getIDAccountbyName(name);
+    }
 
 }
+
