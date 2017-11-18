fe63228574687c6727485a3a9b6d9f3bf13bfc46
TranNgocKhoa <30297271+TranNgocKhoa@users.noreply.github.com>
2017-11-19 00:51:07 +0700

    xem  xoá sửa account

--- app/src/main/java/com/example/khoa1/myapplication/AccountFragment.java
+++ app/src/main/java/com/example/khoa1/myapplication/AccountFragment.java
@@ -9,19 +9,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.khoa1.myapplication.Adapter.AccountAdapter;
 import com.example.khoa1.myapplication.Database.SQLiteAccount;
 import com.example.khoa1.myapplication.Database.SQLiteDataController;
 import com.example.khoa1.myapplication.Model.Account;
 import com.example.khoa1.myapplication.Model.AccountType;
+import com.example.khoa1.myapplication.Model.ChiTieu;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,19 +39,20 @@ public class AccountFragment extends Fragment {
     private ListView lvAccount;
     private ArrayList<Account> listAccount;
     private FloatingActionButton fabThemAccount;
+    private SQLiteAccount account;
     public AccountFragment() {
     }
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
 
         //Log.d("aaa",Integer.toString(account.size()));
 //        lvAccount = (ListView) rootView.findViewById(R.id.lv_Account);
 //        ArrayAdapter<String> arAdp = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, menuitem);
+        account = new SQLiteAccount(getContext());
         lvAccount = (ListView) rootView.findViewById(R.id.lv_Account);
+        registerForContextMenu(lvAccount);
         fabThemAccount = (FloatingActionButton) rootView.findViewById(R.id.fabAccount);
         fabThemAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,47 +64,14 @@ public class AccountFragment extends Fragment {
         });
         //  ArrayList<Account> arrAccount = new ArrayList<Account>();
         createDB();
-        //Test database
-//        Account acc1 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc2 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc3 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc4 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc5 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc6 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
-//        Account acc7 =new Account(1, "Tài Khoản Ví", 300000.0, 0.0, R.drawable.bank_account);
+
         final SQLiteAccount account1 = new SQLiteAccount(getContext());
-//        listAccount = new ArrayList<Account>();
-//        listAccount.add(acc1);
-//        listAccount.add(acc2);
-//        listAccount.add(acc3);
-//        listAccount.add(acc4);
-//        listAccount.add(acc5);
-//        listAccount.add(acc6);
-//        listAccount.add(acc7);
+
         getListaccount();
 
         AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, listAccount);
         lvAccount.setAdapter(accountAdaper);
 
-//        lvAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Intent intent = new Intent(getActivity().getApplicationContext(),ChinhSuaTaiKhoan.class);
-//                startActivity(intent);
-//            }
-//        });
-//        lvAccount.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//                listAccount.get(position).getName();
-//                Log.d("bbb",listAccount.get(position).getName());
-//                deleteaccount(getAccountID(listAccount.get(position).getName()));
-//                getListaccount();
-//                AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, listAccount);
-//                lvAccount.setAdapter(accountAdaper);
-//                return false;
-//            }
-//        });
         lvAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -125,6 +97,48 @@ public class AccountFragment extends Fragment {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Lựa chọn:");
+        //groupId, itemId, order, title
+        menu.add(0, v.getId(), 0, "Xem");
+        menu.add(0, v.getId(), 0, "Sửa");
+        menu.add(0, v.getId(), 0, "Xoá");
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        //Lay vi tri click
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int index = info.position;
+        if(item.getTitle()=="Xem"){
+            Intent intent = new Intent(getActivity().getApplicationContext(),ChiTietTaiKhoan.class);
+            //Truyen vao ma chi tieu cho Activity Chi Tiet Hoat Dong
+            intent.putExtra("Ma Tai Khoan",((Account)lvAccount.getItemAtPosition(index)).getMaTaiKhoan());
+            startActivity(intent);
+        }
+        else if(item.getTitle()=="Sửa"){
+            Intent intent = new Intent(getActivity().getApplicationContext(),ChinhSuaTaiKhoan.class);
+            //Truyen vao ma chi tieu cho Activity Chi Tiet Hoat Dong
+            intent.putExtra("Ma Tai Khoan",((Account)lvAccount.getItemAtPosition(index)).getMaTaiKhoan());
+            intent.putExtra("Them", false);
+            startActivityForResult(intent, 1);
+            Toast.makeText(getContext(),"Sửa",Toast.LENGTH_LONG).show();
+        }
+        else if(item.getTitle()=="Xoá"){
+            account.deleteAccount(((Account)lvAccount.getItemAtPosition(index)).getMaTaiKhoan());
+            getListaccount();
+            AccountAdapter accountAdaper = new AccountAdapter(getActivity(), R.layout.account_listview, listAccount);
+            lvAccount.setAdapter(accountAdaper);
+            Toast.makeText(getContext(),"Xoá",Toast.LENGTH_LONG).show();
+        }
+        else{
+            return false;
+        }
+        return true;
     }
 
     public void deleteaccount(int position){
